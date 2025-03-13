c06ee0fe1b09e05df656313fc2f7b04b1a6c68bc
marienatlandberg <marie.berg2@bergen.kommune.no>
2025-03-13 14:24:30 +0100

    Added footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,16 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  const date = new Date().toLocaleString()
+
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>
+          Page rendered on{' '}
+          {date}
+        </footer>
       </body>
     </html>
   );
